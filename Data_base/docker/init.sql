88005025a80569ccbad30154beea4c35453d09e3
660710180Anathor01 <theerapongworking343@gmail.com>
2025-11-28 15:16:31 +0000

    add database

--- Data_base/docker/init.sql
+++ Data_base/docker/init.sql
@@ -1,108 +1,100 @@
--- Table: user
-CREATE TABLE user (
-    user_id         VARCHAR(255) NOT NULL PRIMARY KEY,
-    libray_id       int,
-    user_name       VARCHAR(255) NOT NULL,
-    email           VARCHAR(255) NOT NULL PRIMARY KEY,
-    phone           VARCHAR(10) NOT NULL,
-    password        VARCHAR(255) NOT NULL,
-   
-    islogin         BOOLEAN DEFAULT FALSE,
-    created_at      TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-
-    FOREIGN KEY (libray_id) REFERENCES librays(libray_id) ON DELETE CASCADE
-);
-
--- Table: amdin
-CREATE TABLE amdin (
-    amdin_id        SERIAL PRIMARY KEY,
-    admin_name      VARCHAR(255) NOT NULL,
-    email           VARCHAR(255) NOT NULL UNIQUE,
-    phone           VARCHAR(10) NOT NULL,
-    password        VARCHAR(255) NOT NULL,
-    created_at      TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-
-    islogin         BOOLEAN DEFAULT FALSE,
-    
-);
-
 -- Table: company
 CREATE TABLE company (
-    company_id          SERIAL PRIMARY KEY,
-    company_name        VARCHAR(255) NOT NULL,
-    company_email       VARCHAR(255) NOT NULL UNIQUE
+    company_id      SERIAL PRIMARY KEY,
+    company_name    VARCHAR(255) NOT NULL,
+    company_email   VARCHAR(255) NOT NULL UNIQUE
 );
 
--- Table: librays
-CREATE TABLE librays (
-    libray_id       SERIAL PRIMARY KEY,
-    game_id         VARCHAR(100) NOT NULL,
-    created_at      TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-
-    FOREIGN KEY (game_id) REFERENCES game(game_id) ON DELETE CASCADE
-    
-);
 
 -- Table: game
 CREATE TABLE game (
     game_id         SERIAL PRIMARY KEY,
     game_name       VARCHAR(255) NOT NULL,
     game_type       VARCHAR(255),
-    company_id      VARCHAR(255) NOT NULL UNIQUE,
+    company_id      INT NOT NULL,
     created_at      TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at      TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     
     FOREIGN KEY (company_id) REFERENCES company(company_id) ON DELETE CASCADE
 );
 
--- Table: bill
-CREATE TABLE bill (
-    bill_id         SERIAL PRIMARY KEY,
-    user_id         VARCHAR(255) NOT NULL,
-    price           float,
+INSERT INTO game (game_name, game_type, company_id) VALUES
+    ('AAA','fps, co-op',1),
+    ('Biohazee','fps, horror',1);
+
+
+
+-- Table: libray
+CREATE TABLE libray (
+    libray_id       SERIAL PRIMARY KEY,
+    game_id         INT NOT NULL,
     created_at      TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES user(user_id) ON DELETE CASCADE
+
+    FOREIGN KEY (game_id) REFERENCES game(game_id) ON DELETE CASCADE
 );
---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
--- Insert user
-INSERT INTO user (user_name, email, password, libray_id, phone) VALUES
-    ('chai', 'somchai@gmail.com', '123', '3', '1'),
-    ('yain',, 'somyain@gmail.com', '456', '4', '1');
-
-        amdin_id        SERIAL PRIMARY KEY,
+-- Table: app_user
+CREATE TABLE app_user (
+    user_id         SERIAL PRIMARY KEY,
+    libray_id       INT NOT NULL,
+    user_name       VARCHAR(255) NOT NULL,
+    email           VARCHAR(255) NOT NULL UNIQUE,
+    phone           VARCHAR(10) NOT NULL,
+    password        VARCHAR(255) NOT NULL,
+    payment_date    DATE,
+    islogin         BOOLEAN DEFAULT FALSE,
+    created_at      TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     
+    FOREIGN KEY (libray_id) REFERENCES libray(libray_id) ON DELETE CASCADE
+);
+
+
+-- Table: admin
+CREATE TABLE admin (
+    admin_id        SERIAL PRIMARY KEY,
     admin_name      VARCHAR(255) NOT NULL,
     email           VARCHAR(255) NOT NULL UNIQUE,
     phone           VARCHAR(10) NOT NULL,
     password        VARCHAR(255) NOT NULL,
-    created_at      TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-
     islogin         BOOLEAN DEFAULT FALSE,
-    FOREIGN KEY (libray_id) REFERENCES librays(libray_id) ON DELETE CASCADE
+    created_at      TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
 
--- Insert admin
-INSERT INTO admin (first_name, last_name, birth_day, email, history) VALUES
-    ('mon', 'monnie@gmail.com', '321', '1'),
-    ('nee', 'nnnn@gmail.com', '654', '1');
 
--- Insert HR
-INSERT INTO hr (first_name, last_name, email, phone) VALUES
-    ('ม่อน', 'รวยป่าว', 'monnie@gmail.com', '0926325624'),
-    ('หนึ่ง', 'ไม้รวย', 'nnnn@gmail.com', '0957468742');
+-- Table: bill
+CREATE TABLE bill (
+    bill_id         SERIAL PRIMARY KEY,
+    user_id         INT NOT NULL,
+    price           FLOAT,
+    created_at      TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    
+    FOREIGN KEY (user_id) REFERENCES app_user(user_id) ON DELETE CASCADE
+);
 
--- Insert Applicants (แปลงวันที่ + แก้วันผิด เช่น 30 ก.พ.)
-INSERT INTO applicants (first_name, last_name, birth_day, email, phone) VALUES
-    ('สมชาย', 'รวยน้อย', '1997-12-12', 'somchai@gmail.com', '0922145624'),
-    ('สมหญิง', 'รวยมาก', '1987-02-28', 'somyain@gmail.com', '0957464567');
 
--- Insert Applications
-INSERT INTO apply (position, file, stage, applicant_id) VALUES
-    ('พนักงานล้างรถ', decode('U29tZSBkYXRh', 'base64'), 'สมัครแล้ว', 1),
-    ('พนักงานบริการ', decode('U29tZSBvdGhlciBkYXRh', 'base64'), 'ผ่านสัมภาษณ์', 2);
+---------------------------------------------------------
+-- Insert data
+---------------------------------------------------------
 
--- Insert Schedule
-INSERT INTO schedule (first_name, last_name, time_s, applicant_id) VALUES
-    ('สมชาย', 'รวยน้อย', '10:00', 1),
-    ('สมหญิง', 'รวยมาก', '11:30', 2);
+INSERT INTO company (company_name, company_email) VALUES
+    ('xgo','xgo@gmail.com'),
+    ('zgo','zgo@gmail.com');
+
+INSERT INTO libray (game_id) VALUES
+    (1),
+    (2);
+
+INSERT INTO user (user_name, email, password, libray_id, phone) VALUES
+    ('chai','somchai@gmail.com','123',1,'321321'),
+    ('yain','somyain@gmail.com','456',2,'123123');
+
+INSERT INTO admin (admin_name, email, phone, password) VALUES
+    ('mon','mon@gmail.com','1234567890','321'),
+    ('nee','nee@gmail.com','9876543210','654');
+
+
+INSERT INTO bill (user_id, price) VALUES
+    (1,100),
+    (2,100);
+
