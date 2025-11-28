60ac3b47cc47a62de69d8da98793fef4973974ce
660710180Anathor01 <theerapongworking343@gmail.com>
2025-11-28 15:55:23 +0000

    fixed game table

--- Data_base/docker/init.sql
+++ Data_base/docker/init.sql
@@ -11,6 +11,7 @@ CREATE TABLE game (
     game_id         SERIAL PRIMARY KEY,
     game_name       VARCHAR(255) NOT NULL,
     game_type       VARCHAR(255),
+    icon           VARCHAR(255),
     company_id      INT NOT NULL,
     created_at      TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at      TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
@@ -18,15 +19,10 @@ CREATE TABLE game (
     FOREIGN KEY (company_id) REFERENCES company(company_id) ON DELETE CASCADE
 );
 
-INSERT INTO game (game_name, game_type, company_id) VALUES
-    ('AAA','fps, co-op',1),
-    ('Biohazee','fps, horror',1);
 
-
-
--- Table: libray
-CREATE TABLE libray (
-    libray_id       SERIAL PRIMARY KEY,
+-- Table: library
+CREATE TABLE library (
+    library_id       SERIAL PRIMARY KEY,
     game_id         INT NOT NULL,
     created_at      TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
 
@@ -37,7 +33,7 @@ CREATE TABLE libray (
 -- Table: app_user
 CREATE TABLE app_user (
     user_id         SERIAL PRIMARY KEY,
-    libray_id       INT NOT NULL,
+    library_id      INT NOT NULL,
     user_name       VARCHAR(255) NOT NULL,
     email           VARCHAR(255) NOT NULL UNIQUE,
     phone           VARCHAR(10) NOT NULL,
@@ -46,7 +42,7 @@ CREATE TABLE app_user (
     islogin         BOOLEAN DEFAULT FALSE,
     created_at      TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     
-    FOREIGN KEY (libray_id) REFERENCES libray(libray_id) ON DELETE CASCADE
+    FOREIGN KEY (library_id) REFERENCES library(library_id) ON DELETE CASCADE
 );
 
 
@@ -81,11 +77,15 @@ INSERT INTO company (company_name, company_email) VALUES
     ('xgo','xgo@gmail.com'),
     ('zgo','zgo@gmail.com');
 
-INSERT INTO libray (game_id) VALUES
+INSERT INTO game (game_name, game_type, icon, company_id) VALUES
+    ('AAA','fps, co-op', "http://...............",1),
+    ('Biohazee','fps, horror', "http://...............",1);
+
+INSERT INTO library (game_id) VALUES
     (1),
     (2);
 
-INSERT INTO user (user_name, email, password, libray_id, phone) VALUES
+INSERT INTO user (user_name, email, password, library_id, phone) VALUES
     ('chai','somchai@gmail.com','123',1,'321321'),
     ('yain','somyain@gmail.com','456',2,'123123');
 
