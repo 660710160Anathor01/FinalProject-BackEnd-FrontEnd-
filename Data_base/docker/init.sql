6212c3478b90660cd763c9d08447301396aa6181
660710180Anathor01 <theerapongworking343@gmail.com>
2025-11-29 10:37:39 +0000

    add_register_feture

--- Data_base/docker/init.sql
+++ Data_base/docker/init.sql
@@ -38,7 +38,7 @@ CREATE TABLE app_user (
     email           VARCHAR(255) NOT NULL UNIQUE,
     phone           VARCHAR(10) NOT NULL,
     password        VARCHAR(255) NOT NULL,
-    payment_date    DATE,
+    payment_date    TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     islogin         BOOLEAN DEFAULT FALSE,
     created_at      TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     
